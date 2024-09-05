e1830ae7a28a465977bc4a14c6273c40b34d4d14
Blais3D <LinwoodBlaisdell@gmail.com>
2024-09-05 13:36:08 -0400

    code transfer from student account

---
+++ README.md
@@ -0,0 +1,5 @@
+# Terrain Generation Using Perlin Noise, Sophistocated Camera Contolling in a 3D space
+
+To execute the project run `python3 build.py` in the 'code' directory to create the executable.
+
+To run this program you must have c++ 17 installed, OpenGl, SDL2, and python 3 installed and configured for your environment.
---
+++ code/src/main.cpp
@@ -0,0 +1,24 @@
+#include "SDLGraphicsProgram.hpp"
+#include "Perlin.hpp"
+#include <iostream>
+
+
+int main(int argc, char** argv){
+
+	std::cout << "Creating perlin objects" << std::endl;
+	Perlin perlin = Perlin();
+
+	std::cout << "generatiing ppms" << std::endl;
+	perlin.generatePPM(false);
+	std::cout << "saving ppm to file" << std::endl;
+
+	perlin.savePPM("", "perlin_texture.ppm");
+
+	// Create an instance of an object for a SDLGraphicsProgram
+	SDLGraphicsProgram mySDLGraphicsProgram(1280,720);
+	// Run our program forever
+	mySDLGraphicsProgram.Loop();
+	// When our program ends, it will exit scope, the
+	// destructor will then be called and clean up the program.
+	return 0;
+}
